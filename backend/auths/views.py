e530dc7dde044a44e4fd4f2e32b8e868e9c7a4ce
CosBett <cosmasbett9@gmail.com>
2022-04-01 07:12:47 +0300

    LogouApi view creation

--- backend/auths/views.py
+++ backend/auths/views.py
@@ -70,3 +70,12 @@ class UserView(APIView):
       
       return Response(serializer.data)
     
+class LogoutView(APIView):
+  def post(self,request):
+    response = Response()
+    response.delete_cookie('jwt')
+    response.data ={
+      'message':'success'
+    }
+    return response   
+    
