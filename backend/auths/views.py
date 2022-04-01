45c061e395383ca3b0c0258a82bdcc63b5dd7646
CosBett <cosmasbett9@gmail.com>
2022-04-01 06:35:38 +0300

    create apiview for login

--- backend/auths/views.py
+++ backend/auths/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+
+from .models import User
 
 
 class RegisterView(APIView):
@@ -10,4 +13,19 @@ class RegisterView(APIView):
    serializer.is_valid(raise_exception=True)
    serializer.save()
    return Response(serializer.data)
- 
+
+class LoginView(APIView):
+  def post(self, request):
+      email = request.data['email']
+      password = request.data['password']
+      
+      user = User.objects.filter(email=email).first()
+      
+      if user is None:
+        raise AuthenticationFailed('User not found')
+      if not user.check_password(password):
+        raise AuthenticationFailed('Password is incorrect')
+      
+      return Response({
+        'message':'success'
+      })
