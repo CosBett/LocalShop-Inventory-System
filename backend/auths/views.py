c4ca38cabe406b2278e453a347f0e5bdd0a7ac59
CosBett <cosmasbett9@gmail.com>
2022-04-01 06:53:32 +0300

    add jwt token

--- backend/auths/views.py
+++ backend/auths/views.py
@@ -3,29 +3,49 @@ from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
+import jwt
+import datetime
 
 from .models import User
 
 
 class RegisterView(APIView):
-  def post(self, request):
-   serializer = UserSerializer(data=request.data)
-   serializer.is_valid(raise_exception=True)
-   serializer.save()
-   return Response(serializer.data)
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
 
 class LoginView(APIView):
-  def post(self, request):
-      email = request.data['email']
-      password = request.data['password']
-      
-      user = User.objects.filter(email=email).first()
-      
-      if user is None:
-        raise AuthenticationFailed('User not found')
-      if not user.check_password(password):
-        raise AuthenticationFailed('Password is incorrect')
-      
-      return Response({
-        'message':'success'
-      })
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        if not user.check_password(password):
+            raise AuthenticationFailed('Password is incorrect')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=120),
+            'iat': datetime.datetime.utcnow()
+        }
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+
+        response.data = {
+            'jwt': token
+        }
+
+        response.data = {
+
+        }
+
+        return response
