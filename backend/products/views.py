f8dd47b09d9b1957d04f0bfe04a8f33bd2494714
CosBett <cosmasbett9@gmail.com>
2022-04-06 03:56:23 +0300

    create viewsets for serializers

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from serializer import ProductSerializer, StockSerializer, OrderPostSerializer, OrderRequestSerializer, StoreSerializer
+from rest_framework import viewsets, permissions
+from .models import Product, Stock, Store, Order_post, Order_request
 
-# Create your views here.
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    permission_classes = [permissions.IsAuthenticated]
+
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    
+    permission_classes = [permissions.IsAuthenticated]  
+    
+class OrderPostViewSet(viewsets.ModelViewSet):
+    queryset = Order_post.objects.all()
+    serializer_class = OrderPostSerializer
+    
+    permission_classes = [permissions.IsAuthenticated]    
+
+class OrderRequestSerializer(viewsets.ModelViewSet):
+    queryset = Order_request.objects.all()
+    serializer_class = OrderRequestSerializer
+    
+    permission_classes = [permissions.IsAuthenticated]    
+    
+class StoreViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+    
+    permission_classes = [permissions.IsAuthenticated]    
+    
