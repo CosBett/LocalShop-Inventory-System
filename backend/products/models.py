fededf92e02d716cd07dfcbbdb57f4ae1b83d58f
CosBett <cosmasbett9@gmail.com>
2022-04-06 03:00:38 +0300

    update order class and create store models

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from auth.models import *
+from auth.models import User, Clerk, Admin
 
 
 class Product(models.Model):
@@ -25,8 +25,7 @@ class Product(models.Model):
     @classmethod
     def all_product(cls):
         return cls.objects.all()
-
-
+    
 class Stock(models.Model):
     PAYMENT_CHOICE = (
         ('paid', 'Paid'),
@@ -57,9 +56,32 @@ class Stock(models.Model):
 
 
 class Order_request(models.Model):
+    order_id = models.IntegerField(primary_key=True, increment=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
     created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.product.name
+
+    def save_request(self):
+        self.save()
+
+    def delete_request(self):
+        self.order()
+
+    @classmethod
+    def all_request(cls):
+        return cls.objects.all()
+
+class Order_post(models.Model):
+    order_id = models.IntegerField(primary_key=True, increment=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    spoilt_quantity = models.IntegerField(default=0)
 
     def __str__(self):
         return self.product.name
@@ -73,3 +95,12 @@ class Order_request(models.Model):
     @classmethod
     def all_orders(cls):
         return cls.objects.all()
+        
+
+class Store(models.Model):  
+    store_id = models.IntegerField(primary_key=True, increment=True)
+    name = models.CharField(max_length=100)
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    clerk = models.ForeignKey(Clerk, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+       
