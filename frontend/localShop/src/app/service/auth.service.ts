7c21ef0662e2b943158c8080670fca34d21a9852
cosBett <cosmasbett9@gmail.com>
2023-06-13 09:06:50 +0300

    packages update

--- frontend/localShop/src/app/service/auth.service.ts
+++ frontend/localShop/src/app/service/auth.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from './api.service';
-import {Observable} from 'rxjs';
-import { tap } from 'rxjs';
+import {Observable} from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
